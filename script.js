75a78c2ddf9597769a4d7ae4b3d135dd5f46cb53
milica11m <32467639+milica11m@users.noreply.github.com>
2019-04-22 02:04:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+document.getElementById('button1').onclick = function(e){
+  document.getElementById("pic1").style.visibility = "hidden";
+}
+document.getElementById('button2').onclick = function(e){
+  document.getElementById("pic2").style.visibility = "hidden";
+}
+document.getElementById('button3').onclick = function(e){
+  document.getElementById("pic3").style.visibility = "hidden";
+}
+document.getElementById('button4').onclick = function(e){
+  document.getElementById("pic4").style.visibility = "hidden";
+}
+document.getElementById('button5').onclick = function(e){
+  document.getElementById("pic5").style.visibility = "hidden";
+}
+document.getElementById('button6').onclick = function(e){
+  document.getElementById("pic6").style.visibility = "hidden";
+}
+document.getElementById('button7').onclick = function(e){
+  document.getElementById("pic7").style.visibility = "hidden";
+}
+document.getElementById('button8').onclick = function(e){
+  document.getElementById("pic8").style.visibility = "hidden";
+}
+document.getElementById('button9').onclick = function(e){
+  document.getElementById("pic9").style.visibility = "hidden";
+}
+
+document.getElementById('more1').onclick = function(e){
+	if (document.getElementById("saznajvise1").style.display === "block") {
+		document.getElementById("saznajvise1").style.display = "none";
+	} else {
+		document.getElementById("saznajvise1").style.display = "block"
+	} 
+}
+document.getElementById('more2').onclick = function(e){
+	if (document.getElementById("saznajvise2").style.display === "block") {
+		document.getElementById("saznajvise2").style.display = "none";
+	} else {
+		document.getElementById("saznajvise2").style.display = "block"
+	} 
+}
